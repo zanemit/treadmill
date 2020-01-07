b59f55b1a7a9085cdce1ae884ab9f093231138df
Zane Mitrevica <zane.mitrevica@gmail.com>
2020-01-07 19:37:48 +0000

    Create camera_trigger.ino

---
+++ camera_trigger.ino
@@ -0,0 +1,20 @@
+// specify the pins of the camera triggers outputs
+int pinCam1 = 13; 
+int pinCam2 = 10;
+
+void setup() {
+  pinMode(pinCam1, OUTPUT);
+  pinMode(pinCam2, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(pinCam1, HIGH);
+  digitalWrite(pinCam2, HIGH);
+  delay(2);  // <- change this to modify framerate
+  
+  digitalWrite(pinCam1, LOW);
+  digitalWrite(pinCam2, LOW);
+  delay(3); // <- change this to modify framerate
+  // currently 200 fps
+}
