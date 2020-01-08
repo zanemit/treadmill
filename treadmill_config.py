b1bbb7c886509c5c410d57397eb24ebb28c607ae
Zane Mitrevica <zane.mitrevica@gmail.com>
2020-01-08 10:17:20 +0000

    Update treadmill_config.py

--- treadmill_config.py
+++ treadmill_config.py
@@ -1,3 +1,7 @@
+"""
+        ONE BASLER CAMERA + TREADMILL SPEED
+"""
+
 from utils.constants import *
 
 # Define a config class with all the options for data acquisition and post-hoc analysis
@@ -8,7 +12,7 @@ class Config:
 
     # ! Change these for every recording
     experiment_folder = "E:\\Zane\\test"   # ? This should be changed for every experiment to avoid overwriting 
-    experiment_name = "test3"  # should be something like YYMMDD_MOUSEID, all files for an experiment will start with this name
+    experiment_name = "test"  # should be something like YYMMDD_MOUSEID, all files for an experiment will start with this name
     experiment_duration = 1*20  # acquisition duration in seconds, alternatively set as None
 
     # * Live video frames display and sensors data plotting
@@ -18,7 +22,7 @@ class Config:
     # * Check that these options are correct
     com_port = "COM5"  # port of the arduino running Firmata for data acquisition
     acquisition_framerate = 200  # fps of camera triggering -> NEED TO SPECIFY SLEEP TIME IN ARDUINO for frame triggering
-    # So this number is just indicative but the true acquisition rate depends on the trigger arduino
+    # this number is just indicative as the true acquisition rate depends on the trigger arduino
 
     overwrite_files = True # ! ATTENTION: this is useful for debug but could lead to overwriting experimental data
     
@@ -29,7 +33,7 @@ class Config:
     """
     analysis_config = { 
         "data_folder": "D:\\Zane\\Plot-videos DLC", # where the data to analyse are stored
-        "experiment_name": "140819_M1R2",
+        "experiment_name": "test",
         "plot_colors": { "emg1":pink, 
                         "emg2":peach, 
                         "emg3":aqua, 
@@ -51,10 +55,7 @@ class Config:
                     "-framerate": "10", #   output video framerate 
                     # TODO this doesnt work FPS
                 },
-
-
     }
-
 
     """
         ############## LIVE PLOTTING  ####################
@@ -62,8 +63,6 @@ class Config:
     live_plotting_config = {
         "n_timepoints": 100, # number of frames to show in the plot
     }
-
-
 
     """
         ############## CAMERA CONFIG  ####################
@@ -86,21 +85,9 @@ class Config:
             "frame_offset_x": "672",
         },
 
-
         # all commands and options  https://gist.github.com/tayvano/6e2d456a9897f55025e25035478a3a50
         # pixel formats https://ffmpeg.org/pipermail/ffmpeg-devel/2007-May/035617.html
 
-        # "outputdict":{ # for ffmpeg
-        #     "-vcodec": "mpeg4",   #   low fps high res
-        #     '-crf': '0',
-        #     '-preset': 'slow',  # TODO check this
-        #     '-pix_fmt': 'gray',  # yuvj444p
-        #     "-cpu-used": "1",  # 0-8. defailt 1, higher leads to higher speed and lower quality
-        #     # "-r": "100", #   output video framerate 
-        #     "-flags":"gray",
-        #     # "-ab":"0",
-        #     # "-force_fps": "100"
-        # },
         "outputdict":{
             "-c:v": 'libx264',   #   low fps high res
             "-crf": '17',
@@ -120,23 +107,9 @@ class Config:
     # Arduino (FIRMATA) setup options
     # * Only change these if you change the configuration of the inputs coming onto the firmata arduino
     arduino_config = {
-       // "emg_pins": {
-       //     # Specify the pins receiving the input from the electrodes
-         //   "emg1": 6, # pwm pins
-           // "emg2": 7,
-            //"emg3": 8, 
-            //"emg4": 9,
-        //},
-        "trdmSpeed": 11,
-        //"arduino_csv_headers": ["frame_number", "elapsed", "camera_timestamp", \
-                           // "trdmSpeed","emg1", "emg2", "emg3", "emg4"],
-        "arduino_csv_headers": ["frame_number", "elapsed", "camera_timestamp", \
-                            "trdmSpeed"],
-        //"emgs": [ "emg1", "emg2", "emg3", "emg4"],
+        "trdmSpeed_pin": 11,
+        "arduino_csv_headers": ["frame_number", "elapsed", "camera_timestamp", "trdmSpeed"],
     }
-
-    emg_names =  [ "emg1", "emg2", "emg3", "emg4"]
-    n_emg = 4 
 
     def __init__(self): 
         return # don't need to do anything but we need this func
