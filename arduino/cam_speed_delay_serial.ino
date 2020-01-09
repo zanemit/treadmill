c0052e925af1156f8d6636fc36dc41e7805db394
Zane Mitrevica <zane.mitrevica@gmail.com>
2020-01-09 20:42:51 +0000

    Update and rename camera_serial.ino to cam_speed_delay_serial.ino

--- arduino/cam_speed_delay_serial.ino
+++ arduino/cam_speed_delay_serial.ino
@@ -5,12 +5,6 @@ int pinCam1 = 10; //basler
 // specify the pin of treadmill speed input
 int trdmSpeed_pin = A0;
 
-// specify the pins of electrode inputs
-//int emg1_pin = 6;
-//int emg2_pin = 7;
-//int emg3_pin = 8;
-//int emg4_pin = 9;
-
 int trdmSpeed_readout = 0;
 
 void setup() {
@@ -18,11 +12,6 @@ void setup() {
   //pinMode(pinCam2, OUTPUT);
   
   pinMode(trdmSpeed_pin, INPUT); 
-
-  //pinMode(emg1_pin, INPUT);
-  //pinMode(emg2_pin, INPUT);
-  //pinMode(emg3_pin, INPUT);
-  //pinMode(emg4_pin, INPUT);
 
  Serial.begin(115200);
 }
@@ -34,18 +23,12 @@ void loop() {
   
   trdmSpeed_readout = analogRead(trdmSpeed_pin);
 
-  //emg1_readout = digitalRead(emg1_pin);
-  //emg2_readout = digitalRead(emg2_pin);
-  //emg3_readout = digitalRead(emg3_pin);
-  //emg4_readout = digitalRead(emg4_pin);
-  
+
   Serial.println(String(1.0)+";"+String(trdmSpeed_readout));
-  //Serial.println(String(1.0)+";"+String(trdmSpeed_readout)+";"+String(emg1_readout)+";"+String(emg2_readout)+";"+String(emg3_readout)+";"+String(emg4_readout));
 
   digitalWrite(pinCam1, LOW);
   //digitalWrite(pinCam2, LOW);
   delay(3); // <- change this to modify framerate
   
   Serial.println(String(0.0)+";"+String(trdmSpeed_readout));
- // Serial.println(String(0.0)+";"+String(trdmSpeed_readout)+";"+String(emg1_readout)+";"+String(emg2_readout)+";"+String(emg3_readout)+";"+String(emg4_readout));
 }
