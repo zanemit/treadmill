f88a1c55bdb992521b60c7af766b12029b4705f3
Zane Mitrevica <zane.mitrevica@gmail.com>
2020-01-14 16:10:03 +0000

    changed camera on/off times

--- arduino/cam_speed_millis_firmata.ino
+++ arduino/cam_speed_millis_firmata.ino
@@ -1,19 +1,19 @@
 #include <Firmata.h>
 
 // specify the pins of the camera triggers outputs
-int pinCam1 = 10; //BaslerCam
-//int pinCam2 = 12; //XimeaCam
+int pinCam1 = 10; //ximea
+int pinCam2 = 11; //basler
 
 // specify the pin of treadmill speed input
-byte trdmSpeed_pin = 0; // is this A0??
+byte trdmSpeed_pin = 0;
 
 // define temporal variables
 unsigned long oldCamMillis = 0;
 unsigned long oldTrdmSpeedMillis = 0;
 unsigned long currentMillis = 0;
   
-int cameraOnEnd = 2; // on for 2 milliseconds
-int cameraOffEnd = 5; // ~ TTL period, camera off for 3 milliseconds
+int cameraOnEnd = 1; // on for 2 milliseconds
+int cameraOffEnd = 4; // ~ TTL period, camera off for 3 milliseconds
 
 void analogWriteCallback(byte pin, int value){
   pinMode(pin, OUTPUT);
@@ -22,11 +22,8 @@ void analogWriteCallback(byte pin, int value){
 
 void setup() {
   pinMode(pinCam1, OUTPUT);
-  //pinMode(pinCam2, OUTPUT);
-  
-  //pinMode(trdmSpeed_pin, INPUT); 
+  pinMode(pinCam2, OUTPUT);
 
-  //Serial.begin(115200);
   Firmata.setFirmwareVersion(FIRMATA_FIRMWARE_MAJOR_VERSION, FIRMATA_FIRMWARE_MINOR_VERSION);
   Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
   Firmata.begin(57600);
@@ -39,12 +36,12 @@ void loop() {
   // control cameras
   if (currentMillis - oldCamMillis < cameraOnEnd){
     digitalWrite(pinCam1, HIGH); //turn BaslerCam on
-    //digitalWrite(pinCam2, HIGH); //turn XimeaCam on
+    digitalWrite(pinCam2, HIGH); //turn XimeaCam on
   }
   
   else if (currentMillis - oldCamMillis < cameraOffEnd){
     digitalWrite(pinCam1, LOW); //turn BaslerCam off
-    //digitalWrite(pinCam2, LOW); //turn XimeaCam off
+    digitalWrite(pinCam2, LOW); //turn XimeaCam off
   }
   
   else{
@@ -56,8 +53,6 @@ void loop() {
   if (currentMillis - oldTrdmSpeedMillis >= 5){
     Firmata.sendAnalog(trdmSpeed_pin, analogRead(trdmSpeed_pin));  
   
-    //int trdmSpeed_readout = analogRead(trdmSpeed_pin); //could delete?
-    //Serial.println(String(1.0)+";"+String(trdmSpeed_readout)); //could delete?
     oldTrdmSpeedMillis = currentMillis;
   }  
 }
