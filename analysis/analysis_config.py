358a6071294ddae77674b5e23f720664a12b55f5
Zane Mitrevica <zane.mitrevica@gmail.com>
2020-01-23 10:39:58 +0000

    Update and rename trdm_speed_test.py to analysis_config.py

---
+++ analysis/analysis_config.py
@@ -0,0 +1,22 @@
+from utils.colors import *
+
+class Config:
+    analysis_config = { 
+        "experimenter" : 'ZM', # initials
+        "experiment_date" : '200115', # format 'YYMMDD'
+        "experiment_type" : 'V', # 'V' = videos, 'E' = EMG
+        "dlc_date" : 'Jan16', # format month-date like the created DLC folder
+        "dlc_iters" : '5000',
+
+        "data_folder": "W:\\murray\\Zane", # where the data to analyse are stored
+        "experimenter": "ZM",  # initials
+        "experiment_date": "200115",  # format 'YYMMDD'
+        "experiment_type": "V", # 'V' = videos, 'E' = EMG
+        "dlc_project_date": "Jan16", 
+        "dlc_iters": "5000",      
+        "plot_colors": { "emg1":pink, 
+                        "emg2":peach, 
+                        "emg3":aqua, 
+                        "emg4":purple},
+
+    }
