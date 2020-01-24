c13ad37e19a7eef1d99202826dc1e8e2db590c1c
Zane Mitrevica <zane.mitrevica@gmail.com>
2020-01-24 07:24:29 +0000

    Update analysis_config.py

--- analysis/analysis_config.py
+++ analysis/analysis_config.py
@@ -1,22 +1,20 @@
-from utils.colors import *
-
 class Config:
     analysis_config = { 
-        "experimenter" : 'ZM', # initials
-        "experiment_date" : '200115', # format 'YYMMDD'
-        "experiment_type" : 'V', # 'V' = videos, 'E' = EMG
-        "dlc_date" : 'Jan16', # format month-date like the created DLC folder
-        "dlc_iters" : '5000',
-
         "data_folder": "W:\\murray\\Zane", # where the data to analyse are stored
         "experimenter": "ZM",  # initials
         "experiment_date": "200115",  # format 'YYMMDD'
         "experiment_type": "V", # 'V' = videos, 'E' = EMG
+        "mouse_id": "518.3", 
         "dlc_project_date": "Jan16", 
-        "dlc_iters": "5000",      
-        "plot_colors": { "emg1":pink, 
-                        "emg2":peach, 
-                        "emg3":aqua, 
-                        "emg4":purple},
-
+        "dlc_iters": "200000",
+    }
+    
+    colors = { 
+        "purple" : [94/255,79/255,162/255],
+        'aqua' : [102/255, 194/255, 165/255],
+        'green' : [176/255, 236/255, 87/255],
+        'pink' : [213/255, 62/255, 79/255],
+        'peach' : [253/255, 174/255, 97/255],
+        'white' : [1., 1., 1.],
+        'grey' : [.7, .7, .7]
     }
